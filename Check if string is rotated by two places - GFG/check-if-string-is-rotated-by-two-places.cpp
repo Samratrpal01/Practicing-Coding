7c2611bfaf51f9a378e20cbff00964246d55807a
TonyStarkRp01 <55346697+Samratrpal01@users.noreply.github.com>
2022-05-31 13:58:56 +0530

    Added solution - LeetHub

---
+++ Check if string is rotated by two places - GFG/check-if-string-is-rotated-by-two-places.cpp
@@ -0,0 +1,44 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution
+{
+    public:
+    //Function to check if a string can be obtained by rotating
+    //another string by exactly 2 places.
+    bool isRotated(string str1, string str2)
+    {
+      string s = str1 + str1;
+       string r = str2 + str2;
+       if(s.find(str2) == 2) {
+           return true;
+       }
+       if(r.find(str1) == 2) {
+           return true;
+       }
+       return false;
+    }
+
+};
+
+
+// { Driver Code Starts.
+
+int main() {
+	
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		string s;
+		string b;
+		cin>>s>>b;
+		Solution obj;
+		cout<<obj.isRotated(s,b)<<endl;
+	}
+	return 0;
+}
+  // } Driver Code Ends
