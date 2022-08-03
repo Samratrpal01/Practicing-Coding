0473ba96e4a82e838b5d7ab0ff91ff28d719937e
TonyStarkRp01 <55346697+Samratrpal01@users.noreply.github.com>
2022-08-03 13:16:19 +0530

    Time: 4 ms (66.43%), Space: 8 MB (27.59%) - LeetHub

--- 46-permutations/46-permutations.cpp
+++ 46-permutations/46-permutations.cpp
@@ -1,14 +1,31 @@
 class Solution {
 public:
+     vector<int>permutations;
+    int n;
+    bool chosen[10];
+   
+    void search(vector<vector<int>>&result,vector<int>nums)
+    {
+        if(permutations.size()==n)
+            result.push_back(permutations);
+        else
+        {   for(int i=0;i<n;i++)
+        {   if(chosen[i])
+                continue;
+            chosen[i]=true;
+            permutations.push_back(nums[i]);
+            search(result,nums);
+            chosen[i]=false;
+            permutations.pop_back();
+        }
+        }
+    }
+   
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<vector<int>>v;
-        sort(nums.begin(),nums.end());
-        do
-        {
-            v.push_back(nums);
-        }while(next_permutation(nums.begin(),nums.end()));
-        
-        return v;
+      n=nums.size();
+         vector<vector<int>>result;
+        search(result,nums);
+        return result;
         
     }
 };
