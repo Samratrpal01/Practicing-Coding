4a4db64870dae0ce6cc3ff538093f0c74536a0fd
TonyStarkRp01 <55346697+Samratrpal01@users.noreply.github.com>
2022-08-04 01:22:12 +0530

    Time: 5 ms (31.84%), Space: 6.4 MB (53.91%) - LeetHub

--- 790-domino-and-tromino-tiling/790-domino-and-tromino-tiling.cpp
+++ 790-domino-and-tromino-tiling/790-domino-and-tromino-tiling.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    int numTilings(int n) {
-       if(n < 3){
-            return n;
-        }
-
-       vector<int>arr(n+1);
-        arr[0] = 0;
-        arr[1] = 1;
-        arr[2] = 2;
-        arr[3] = 5;
-        
-        for(int i = 4;i<=n;i++){
-            arr[i] = ((2 * arr[i-1])%1000000007 + arr[i-3]%1000000007)%1000000007;
-        }
-        return arr[n];  
+    int numTilings(int N) {
+      int md=1e9;
+    md+=7;
+    vector<long long> v(1001,0);
+    v[1]=1;
+    v[2]=2;
+    v[3]=5;
+    if(N<=3)
+        return v[N];
+    for(int i=4;i<=N;++i){
+        v[i]=2*v[i-1]+v[i-3]; 
+        v[i]%=md;
+    }
+    return v[N];
+    
     }
 };
