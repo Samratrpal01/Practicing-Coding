8b0d2e822a9dbe8d8526173b34db907a9bfa1b84
TonyStarkRp01 <55346697+Samratrpal01@users.noreply.github.com>
2022-08-03 11:48:32 +0530

    Time: 115 ms (70.08%), Space: 14.4 MB (72.93%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -3,15 +3,17 @@ public:
     int coinChange(vector<int>& coins, int amount) {
         vector<int>need(amount+1,amount+1);
         need[0]=0;
-        for(int c:coins)
-        {
-            for(int a=c;a<=amount;a++)
+        for(auto x:coins)
+        for(int i=x;i<=amount;i++)    
             {
-                need[a]=min(need[a],need[a-c]+1);
-                
+              
+                    need[i]=min(need[i],need[i-x]+1);
             }
-        }
+        
+        
         return need.back()>amount?-1:need.back();
+     
+      
     }
 };
  
