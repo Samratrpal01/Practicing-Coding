1a347f3cbc59f75a4ed7f7d4bc7b8f3b28d90903
TonyStarkRp01 <55346697+Samratrpal01@users.noreply.github.com>
2022-08-05 15:15:04 +0530

    Time: 35 ms (34.44%), Space: 16.5 MB (23.33%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     vector<vector<int>>res;
-    void dfs(vector<vector<int>>graph,vector<int>path,int start,int end)
+    void dfs(vector<vector<int>>&graph,vector<int>path,int start,int end)
     {path.push_back(start);
         if(start==end)
         {res.push_back(path);
