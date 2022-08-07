feee3a9844d0bb208096f839cec66ffae1fbada8
TonyStarkRp01 <55346697+Samratrpal01@users.noreply.github.com>
2022-08-07 22:40:38 +0530

    Time: 55 ms (91.31%), Space: 19.1 MB (55.11%) - LeetHub

--- 477-total-hamming-distance/477-total-hamming-distance.cpp
+++ 477-total-hamming-distance/477-total-hamming-distance.cpp
@@ -1,23 +1,14 @@
 class Solution {
 public:
     int totalHammingDistance(vector<int>& nums) {
-       int size = nums.size();
-        if(size < 2) return 0;
-        int ans = 0;
-        int *zeroOne = new int[2];
-        while(true)
-        {
-            int zeroCount = 0;
-            zeroOne[0] = 0;
-            zeroOne[1] = 0;
-            for(int i = 0; i < nums.size(); i++)
-            {
-                if(nums[i] == 0) zeroCount++;
-                zeroOne[nums[i] % 2]++;
-                nums[i] = nums[i] >> 1;
+      int n=nums.size(), ans=0;
+        for(int i=0;i<32;i++){
+            int count=0; //Count of the no.of elements that have the 'i'th bit set
+            for(int k=0;k<n;k++){
+                count += (nums[k]>>i)&1;
             }
-            ans += zeroOne[0] * zeroOne[1];
-            if(zeroCount == nums.size()) return ans;
+            ans += count*(n-count);
         }
+        return ans;
     }
 };
