3271d31bbe28dc9c8568379f82014bddaea2a39f
TonyStarkRp01 <55346697+Samratrpal01@users.noreply.github.com>
2022-08-12 00:56:20 +0530

    Time: 6 ms (11.24%), Space: 5.9 MB (79.78%) - LeetHub

--- 793-preimage-size-of-factorial-zeroes-function/793-preimage-size-of-factorial-zeroes-function.cpp
+++ 793-preimage-size-of-factorial-zeroes-function/793-preimage-size-of-factorial-zeroes-function.cpp
@@ -1,17 +1,12 @@
 class Solution {
 public:
     int preimageSizeFZF(int K) {
-     for (int l = 0, r = INT_MAX; l < r;) {
-        int m = l + (r - l) / 2, c = m;
-        for (int i = 5; m / i > 0; i *= 5)
-            c += m / i; // the m-th is c
-        if (c < K)
-            l = m + 1;
-        else if (c > K)
-            r = m;
-        else
-            return 5;
-    }
-    return 0;   
+     int sum[13]={1};
+        for (int i=1; i<13; i++) sum[i]=sum[i-1]*5+1;
+        for (int i=12; i>=0; i--) {
+            if (K/sum[i]==5) return 0;
+            K=K%sum[i];
+        }
+        return 5;
     }
 };
