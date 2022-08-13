14beb458df8ed686c852465c9e1682dec428c603
TonyStarkRp01 <55346697+Samratrpal01@users.noreply.github.com>
2022-08-14 03:01:40 +0530

    Time: 585 ms (39.80%), Space: 105.1 MB (58.16%) - LeetHub

--- 1157-online-majority-element-in-subarray/1157-online-majority-element-in-subarray.cpp
+++ 1157-online-majority-element-in-subarray/1157-online-majority-element-in-subarray.cpp
@@ -1,72 +1,45 @@
 class MajorityChecker {
-public: unordered_map<int, vector<int>> mp;
-    vector<int> arr, node;
+public:
+   unordered_map<int, vector<int>> pos;
+    vector<int> a;
+    int try_bound;
     
-   MajorityChecker(vector<int>& arr) : arr(arr) {
-        if (arr.size() == 0) return ;
+public:
+    MajorityChecker(vector<int>& arr): a(arr) {
+        srand(time(nullptr));
         for (int i = 0; i < arr.size(); ++i) {
-            if (mp.find(arr[i]) == mp.end()) mp[arr[i]] = vector<int>(1, i);
-            else mp[arr[i]].push_back(i);
+            pos[arr[i]].push_back(i);
         }
-        node.resize(4 * arr.size(), -1);
-        build(0, arr.size() - 1, 1);
+        try_bound = 20;
+    }
+    
+    int get_occurrence(int num, int l, int r) {
+        auto iter = pos.find(num);
+        if (iter == pos.end()) {
+            return 0;
+        }
+        const auto& vec = iter->second;
+        auto iter_l = lower_bound(vec.begin(), vec.end(), l);
+        if (iter_l == vec.end()) {
+            return 0;
+        }
+        auto iter_r = upper_bound(vec.begin(), vec.end(), r);
+        return iter_r - iter_l;
+    }
+    
+    int get_random(int l, int r) {
+        return rand() % (r - l + 1) + l;
     }
     
     int query(int left, int right, int threshold) {
-        // index, occur
-        pair<int, int> ret = query(left, right, 0, arr.size() - 1, 1);
-        if (ret.second >= threshold) return ret.first;
-        else return -1;
+        for (int i = 0; i < try_bound; ++i) {
+            int elem = a[get_random(left, right)];
+            if (get_occurrence(elem, left, right) >= threshold) {
+                return elem;
+            }
+        }
+        return -1;
     }
-private:
-    void build(int st, int ed, int cur) {
-        if (st == ed) {
-            node[cur] = arr[st];
-            return ;
-        }
-        int mid = (st + ed) / 2;
-        build(st, mid, cur * 2);
-        build(mid + 1, ed, cur * 2 + 1);
-        if (node[cur * 2] != -1 && count_occur(node[cur * 2], st, ed) * 2 > ed - st + 1) {
-            node[cur] = node[cur * 2];
-        }
-        else if (node[cur * 2 + 1] != -1 && count_occur(node[cur * 2 + 1], st, ed) * 2 > ed - st + 1) {
-            node[cur] = node[cur * 2 + 1];
-        }
-        return ;
-    }
-
-    pair<int, int> query(const int &l, const int &r, int st, int ed, int cur) {
-        if (st > r || ed < l) {
-            return make_pair(-1, -1);
-        }
-        if (l <= st && ed <= r) {
-            if (node[cur] == -1) return make_pair(-1, -1);
-            int occ = count_occur(node[cur], l, r);
-            if (occ * 2 > r - l + 1) return make_pair(node[cur], occ);
-            else return make_pair(-1, -1);
-        }
-        int mid = (st + ed) / 2;
-        if (l <= mid) {
-            pair<int, int> ret_l = query(l, r, st, mid, cur * 2);
-            if (ret_l.first != -1) return ret_l;
-        }
-        if (r > mid) {
-            pair<int, int> ret_r = query(l, r, mid + 1, ed, cur * 2 + 1);
-            if (ret_r.first != -1) return ret_r;
-        }
-        return make_pair(-1, -1);
-    }
-
-    int count_occur(int val, int left, int right) {
-        int st_idx = lower_bound(mp[val].begin(), mp[val].end(), left)
-                - mp[val].begin();
-        int ed_idx = upper_bound(mp[val].begin(), mp[val].end(), right)
-                - mp[val].begin();
-        return ed_idx - st_idx;
-    }
-
-   
 };
 
 /**
