9d0ba15dd76e1af81ad3ed7b8e9e48fb61c8a582
TonyStarkRp01 <55346697+Samratrpal01@users.noreply.github.com>
2022-08-18 03:29:34 +0530

    Time: 3 ms (83.83%), Space: 10.9 MB (78.61%) - LeetHub

---
+++ 993-cousins-in-binary-tree/993-cousins-in-binary-tree.cpp
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int hx,hy,px,py;
+    void fun(TreeNode*root,int l,int x,int y,int prev)
+    {   if(root==NULL)
+            return;
+        if(root->val==x)
+        {
+            hx=l;
+            px=prev;
+        }
+     if(root->val==y)
+     {
+         hy=l;
+         py=prev;
+     }
+        fun(root->left,l+1,x,y,root->val);
+        fun(root->right,l+1,x,y,root->val);
+        
+    }
+    bool isCousins(TreeNode* root, int x, int y) {
+        
+        
+        fun(root,0,x,y,root->val);
+        if(hx==hy and px!=py)
+            return true;
+        else
+        return false;
+        
+        
+        
+    }
+};
