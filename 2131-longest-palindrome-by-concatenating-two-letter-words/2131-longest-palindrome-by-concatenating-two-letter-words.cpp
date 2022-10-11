5cbe50c37c9d80d51535ccd4675cb022b1dc98e4
TonyStarkRp01 <55346697+Samratrpal01@users.noreply.github.com>
2022-10-11 16:14:41 +0530

    Time: 1957 ms (5.05%), Space: 167.8 MB (81.36%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.cpp
@@ -2,36 +2,35 @@ class Solution {
 public:
     int longestPalindrome(vector<string>& words) {
         map<string,int>mp;
-        for(auto w:words)
-        {
-            mp[w]++;
-        }
-        int count=0;
-            bool flag=false;
-            for(auto x:words)
-            {
-                string w=x;
-                reverse(w.begin(),w.end());
-                if(w!=x and mp[x]>0 and mp[w]>0)
-                {
-                    mp[x]--;
-                    mp[w]--;
-                    count+=4;
-                }
-                else if(w==x and mp[x]>1)
-                {
-                    mp[x]-=2;
-                    count+=4;
-                }
-                else if(w==x and !flag  and mp[x]>0)
-                {
-                    mp[x]-=1;
-                    count+=2;
-                    flag=true;
-                }
-            }
-            
-            return count;
+        for(auto x:words)
+            mp[x]++;
         
+        bool flag=false;
+        int count=0;
+        for(auto x:words)
+        {
+            string w=x;
+            reverse(x.begin(),x.end());
+            if(w!=x and mp[x]>0 and mp[w]>0)
+            {
+                mp[x]--;
+                mp[w]--;
+                count+=4;
+            }
+            else if(x==w and mp[x]>1)
+            {
+                mp[x]-=2;
+                count+=4;
+            }
+            else if(x==w and !flag and mp[x]>0)
+            {
+                mp[x]--;
+                count+=2;
+                flag=true;
+            }
+  
+        }
+        
+        return count;
     }
 };
